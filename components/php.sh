d2237d781c688e60e71c18e4b9a39db66986d6e1
Audrius <aubuka@gmail.com>
2015-12-09 22:54:24 +0000

    auto accept php libraries install, restart php service after config edit

--- components/php.sh
+++ components/php.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Set up PHP-FPM.
 
-apt-get install php5-fpm php5-mysql php5-curl php5-mcrypt php5-gd php-pear php5-imagick
+apt-get install php5-fpm php5-mysql php5-curl php5-mcrypt php5-gd php-pear php5-imagick -y
 
 echo "****************** Editing PHP config ******************"
 
@@ -17,3 +17,6 @@ cp /etc/php5/fpm/php.ini /etc/php5/fpm/php.ini.backup
 
 # modify php5-fpm config file.
 sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
+
+# Restart php5-fpm.
+service php5-fpm restart
